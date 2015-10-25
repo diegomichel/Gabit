f5f2d6cb87c06c3b016c64082a38883292b5043b
Diego Michel Rubio Ramirez <diego.rubio@agilekoding.com>
2015-10-24 23:43:29 -0500

    Some refactoring

---
+++ thegabit/website/static/tasks/Botones.js
@@ -0,0 +1,61 @@
+/**
+ * Created with PyCharm.
+ * User: diego
+ * Date: 26/06/13
+ * Time: 05:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+$(function () {
+    $("#trash").droppable({
+        tolerance: "pointer",
+        accept: ".task",
+        hoverClass: "buttonHover",
+        drop: function (event, ui) {
+            $("#dialog-confirm").removeClass("hidden").dialog({
+                resizable: false,
+                height: 140,
+                modal: true,
+                buttons: {
+                    "Yes": function () {
+                        Tasks.delete(ui.draggable.get(0));
+                        ui.draggable.get(0).remove();
+                        $(this).dialog("close");
+                    },
+                    Cancel: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+        }
+    }).hide();
+
+    $("#completed").droppable({
+        tolerance: "pointer",
+        accept: ".task",
+        hoverClass: "buttonHover",
+        drop: function (event, ui) {
+            if (parseInt($(ui.draggable.get(0)).closest("dl").attr("id")[4]) == 0)
+            {
+                $( "#dialog-confirm-todo").data("ui",ui.draggable.get(0)).dialog("open");
+                return;
+            }
+            else{
+                Tasks.complete(ui.draggable.get(0));
+                $(ui.draggable.get(0)).addClass("taskDone");
+                if (parseInt($(ui.draggable.get(0)).closest("dl").attr("id")[4]) == 1)
+                    makeDarker($(ui.draggable.get(0)));
+            }
+        }
+    }).hide();
+
+    $("#reward").droppable({
+        tolerance: "pointer",
+        accept: ".task",
+        hoverClass: "buttonHover",
+        drop: function (event, ui) {
+            Tasks.buyReward(ui.draggable.get(0));
+            //ui.draggable.get(0).remove();
+        }
+    }).hide();
+
+});
